0ad143af61cfa6ec61018eb5013c20872c693952
Adrianxx1 <triaxis12q@gmail.com>
2025-05-27 23:21:48 -0600

    implementacion de footer y home

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer style={{ backgroundColor: '#333', color: '#fff', textAlign: 'center', padding: '10px' }}>
+      <p>© 2025 Mi Sitio Web</p>
+    </footer>
+  )
+}
+
+export default Footer
